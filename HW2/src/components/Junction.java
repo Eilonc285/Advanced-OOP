2dc0a85d6326b71f884a3fba4b2874d11bf4f7cf
Eilonc285 <eiloncohen285@gmail.com>
2020-04-30 16:58:22 +0300

    rearranged code

--- HW2/src/components/Junction.java
+++ HW2/src/components/Junction.java
@@ -6,25 +6,45 @@ import utilities.Point;
 
 public class Junction extends Point implements RouteParts {
 	private int objectCounts;
-	private ArrayList<Road> enteringRoads; 
+	private ArrayList<Road> enteringRoads;
 	private ArrayList<Road> exitingRoads;
 	private String junctionName;
-	
-	public Junction() {}
-	public Junction(String junctionName, double x, double y) {}
-	public void addEnteringRoad(Road road) {}
-	public void addExitingRoad(Road road) {}
+
+	public Junction() {
+	}
+
+	public Junction(String junctionName, double x, double y) {
+	}
+
+	public void addEnteringRoad(Road road) {
+	}
+
+	public void addExitingRoad(Road road) {
+	}
+
 	public double calcEstimatedTime(Object obj) {
-		return objectCounts;}
+		return objectCounts;
+	}
+
 	public boolean canLeave(Vehicle vehicle) {
-		return false;}
+		return false;
+	}
+
 	public boolean checkAvailability(Vehicle vehicle) {
-		return false;}
-	public void checkIn(Vehicle vehicle) {}
-	public void checkOut(Vehicle vehicle) {}
+		return false;
+	}
+
+	public void checkIn(Vehicle vehicle) {
+	}
+
+	public void checkOut(Vehicle vehicle) {
+	}
+
 	public RouteParts findNextPart(Vehicle vehicle) {
-		return null;}
-	public void stayOnCurrentPart(Vehicle vehicle) {}
-	
-	
+		return null;
+	}
+
+	public void stayOnCurrentPart(Vehicle vehicle) {
+	}
+
 }
